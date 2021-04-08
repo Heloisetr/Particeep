59f864a703356e73fd0330b447f9af13114224f9
Héloïse TRONCHE <heloisetronche@iphonedeheloise.home>
2021-04-08 17:41:41 +0200

    Done the fundamental of the project

---
+++ src/Routes.tsx
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+
+//import DashboardFlights from 'containers/dahsboardContainer/DashboardFlightsContainer';
+
+const Routes = () => (
+  <Switch>
+   
+  </Switch>
+);
+
+export default Routes;
